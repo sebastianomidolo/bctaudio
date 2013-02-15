f8497ccf5a52374f438be720c4954f3faaabcbec
Sebastiano Midolo <sebastiano.mdl@gmail.com>
2013-02-15 15:24:05 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bctaudio::Application.config.secret_token = '7db445649b1a1834d01e7aeaa01a7ec38f056690bd19676a0559aebc80b8b7f265f3bd0a5d5c6c2b308983a627e783174e6e078fcd2883dc63f15b674baa66c2'
